352c71c10b88465d11d273a1fe72f4e1cda9e771
ncaleb52 <ncaleb52@gmail.com>
2025-08-28 16:51:38 +0300

    Added missing file/directories

---
+++ smartlaundry-api/src/test/java/ke/co/smartlaundry/SmartlaundryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.co.smartlaundry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlaundryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
