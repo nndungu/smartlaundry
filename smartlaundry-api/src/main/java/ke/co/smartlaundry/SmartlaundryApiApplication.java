e07def07ef85b081e7d7fe77a2a32f481794e445
ncaleb52 <ncaleb52@gmail.com>
2025-08-28 16:51:38 +0300

    Added missing file/directories

---
+++ smartlaundry-api/src/main/java/ke/co/smartlaundry/SmartlaundryApiApplication.java
@@ -0,0 +1,13 @@
+package ke.co.smartlaundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlaundryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlaundryApiApplication.class, args);
+    }
+
+}
