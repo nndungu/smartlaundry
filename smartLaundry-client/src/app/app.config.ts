aaa77de00bb249aa2bc4766a92852d1dce16f30f
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-08-29 14:37:41 +0300

    git rebase --continuecreate register page

--- smartLaundry-client/src/app/app.config.ts
+++ smartLaundry-client/src/app/app.config.ts
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // ← Add this
-=======
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
->>>>>>> de38134 ( initialized client folder)
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()) // ← Add this line
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
-=======
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
-};
->>>>>>> de38134 ( initialized client folder)
