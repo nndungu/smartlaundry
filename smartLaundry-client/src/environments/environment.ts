824388c607591f1030cfdc8c7d0719a5cf64601c
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-09-11 17:20:18 +0300

    chore: add environment configuration files

---
+++ smartLaundry-client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxWmo9EP4gaaN3G9SIS6XTWXP97p8emmY",
+    authDomain: "smart-laundry-d31f3.firebaseapp.com",
+    projectId: "smart-laundry-d31f3",
+    storageBucket: "smart-laundry-d31f3.firebasestorage.app",
+    messagingSenderId: "449810253325",
+    appId: "1:449810253325:web:9943a6b21fa7844413b744",
+    measurementId: "G-SWQS8YEXTW"
+  }
+};
