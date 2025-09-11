02d4b69a40c279d8b423773a7a57afb06a461bdd
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-09-11 17:18:42 +0300

    feat: update app config, firebase setup, and auth guard logic

--- smartLaundry-client/src/app/app.config.ts
+++ smartLaundry-client/src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
+import { firebaseConfig } from './core/firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
   ]
 };
--- smartLaundry-client/src/app/core/guards/auth-guard.ts
+++ smartLaundry-client/src/app/core/guards/auth-guard.ts
@@ -1,15 +1,21 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth';
+import { map, take } from 'rxjs/operators';
 
 export const authGuard = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated()) {
-    return true;
-  }
-
-  router.navigate(['/login']);
-  return false;
+  return authService.isAuthenticated().pipe(
+    take(1),
+    map(isAuthenticated => {
+      if (isAuthenticated) {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    })
+  );
 };
