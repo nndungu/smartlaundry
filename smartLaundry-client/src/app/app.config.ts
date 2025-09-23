16217c9d62c06523add5ac30d6f9d8936b19c652
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-09-23 14:57:09 +0300

    chore(config): update app configuration for profile update changes

--- smartLaundry-client/src/app/app.config.ts
+++ smartLaundry-client/src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 import { firebaseConfig } from './core/firebase.config';
@@ -14,6 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    provideAnimationsAsync(),
+    provideNativeDateAdapter()
   ]
 };
